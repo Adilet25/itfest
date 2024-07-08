53e220a1aeb57e4b5aeb9a58e8656e6ac9ed2795
Adilet <baryktabasovadi@gmail.com>
2024-07-08 04:02:21 +0300

    added smooth slide for partners

--- src/components/Sections/PartnerSection/PartnerSection.jsx
+++ src/components/Sections/PartnerSection/PartnerSection.jsx
@@ -6,20 +6,87 @@ import logo4 from "../../../assets/partnerslogo/logo4.svg";
 import logo5 from "../../../assets/partnerslogo/logo5.svg";
 import logo6 from "../../../assets/partnerslogo/logo6.svg";
 import logo7 from "../../../assets/partnerslogo/logo7.svg";
+import logo8 from "../../../assets/partnerslogo/logo8.svg";
+import logo9 from "../../../assets/partnerslogo/logo9.svg";
+import logo10 from "../../../assets/partnerslogo/logo10.svg";
+import logo11 from "../../../assets/partnerslogo/logo11.svg";
+import logo12 from "../../../assets/partnerslogo/logo12.svg";
+import logo13 from "../../../assets/partnerslogo/logo13.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
 import "./PartnerSection.css";
+import { Autoplay } from "swiper/modules";
 
 const PartnerSection = () => {
   return (
     <div className="bg4">
       <div className="partnerBlock_main">
+        <div className="partnerBlock_mini partnerMini_0">
+          <h2>Генеральный Партнер</h2>
+          <div className="parnterBlock_imgs0">
+            <img src={logo8} alt="error" />
+          </div>
+        </div>
         <div className="partnerBlock_mini partnerMini_1">
           <h2>Партнёры</h2>
-          <div className="parnterBlock_imgs">
-            <img src={logo3} alt="error" />
-            <img src={logo6} alt="error" />
-            <img src={logo7} alt="error" />
+          <div className="parnterBlock_imgs1">
+            <Swiper
+              speed={10000}
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 0,
+                disableOnInteraction: false,
+              }}
+              modules={[Autoplay]}
+              loop={true}
+              freeMode={true}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 50,
+                },
+              }}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <img src={logo9} alt="error" className="swiperImgs" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={logo10} alt="error" className="swiperImgs imgneko" />
+              </SwiperSlide>{" "}
+              <SwiperSlide>
+                <img src={logo11} alt="error" className="swiperImgs" />
+              </SwiperSlide>{" "}
+              <SwiperSlide>
+                <img src={logo12} alt="error" className="swiperImgs" />
+              </SwiperSlide>{" "}
+              <SwiperSlide>
+                <img src={logo13} alt="error" className="swiperImgs" />
+              </SwiperSlide>{" "}
+              <SwiperSlide>
+                <img src={logo3} alt="error" className="swiperImgs" />
+              </SwiperSlide>{" "}
+              <SwiperSlide>
+                <img src={logo6} alt="error" className="swiperImgs" />
+              </SwiperSlide>{" "}
+              <SwiperSlide>
+                <img src={logo7} alt="error" className="swiperImgs imgneko" />
+              </SwiperSlide>
+            </Swiper>
           </div>
+
+          {/* </div> */}
         </div>
         <div className="partnerBlock_mini partnerMini_2">
           <h2>Медиа партнёры</h2>
