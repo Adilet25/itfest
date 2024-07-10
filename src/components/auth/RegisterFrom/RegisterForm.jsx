1ab72535cdcc59a24edc36fa7ad28726c7f14c84
Adilet <baryktabasovadi@gmail.com>
2024-07-10 19:40:49 +0300

    fixed register2

--- src/components/auth/RegisterFrom/RegisterForm.jsx
+++ src/components/auth/RegisterFrom/RegisterForm.jsx
@@ -312,9 +312,9 @@ const RegisterForm = () => {
       };
       register(formdata, fork);
     }
-    // if (checked && inpBtnSt && !errorr) {
-    //   navigate("/");
-    // }
+    if (checked && inpBtnSt && !errorr) {
+      navigate("/");
+    }
   }
   function formatphone(value) {
     if (!value) return value;
